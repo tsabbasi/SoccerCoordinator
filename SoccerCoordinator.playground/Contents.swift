3de26ad4deaf8cb74a07a621e5670773cb4267cd
Tuba Abbasi <tsabbasi@gmail.com>
2016-08-14 16:20:44 -0400

    Generate Letters in Progress

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -2,55 +2,55 @@
 
 import UIKit
 
-// Single Player Data
+// Single Player Data | Dictionaries
 
-let player1Info = ["name": "Joe Smith", "height": "42", "soccerExperience": "YES", "guardianName": "Jim and Jan Smith"]
+var player1Info = ["name": "Joe Smith", "height": "42", "soccerExperience": "YES", "guardianName": "Jim and Jan Smith"]
 
-let player2Info = ["name": "Jill Tanner", "height": "36", "soccerExperience": "YES", "guardianName": "Clara Tanner"]
+var player2Info = ["name": "Jill Tanner", "height": "36", "soccerExperience": "YES", "guardianName": "Clara Tanner"]
 
-let player3Info = ["name": "Bill Bon", "height": "43", "soccerExperience": "YES", "guardianName": "Sara and Jenny Bon"]
+var player3Info = ["name": "Bill Bon", "height": "43", "soccerExperience": "YES", "guardianName": "Sara and Jenny Bon"]
 
-let player4Info = ["name": "Eva Gordon", "height": "45", "soccerExperience": "NO", "guardianName": "Wendy and Mike Gordon"]
+var player4Info = ["name": "Eva Gordon", "height": "45", "soccerExperience": "NO", "guardianName": "Wendy and Mike Gordon"]
 
-let player5Info = ["name": "Matt Gill", "height": "40", "soccerExperience": "NO", "guardianName": "Wendy and Mike Gordon"]
+var player5Info = ["name": "Matt Gill", "height": "40", "soccerExperience": "NO", "guardianName": "Charles and Sylvia Gill"]
 
-let player6Info = ["name": "Kimmy Stein", "height": "41", "soccerExperience": "NO", "guardianName": "Bill and Hillary Stein"]
+var player6Info = ["name": "Kimmy Stein", "height": "41", "soccerExperience": "NO", "guardianName": "Bill and Hillary Stein"]
 
-let player7Info = ["name": "Sammy Adams", "height": "45", "soccerExperience": "NO", "guardianName": "Jeff Adams"]
+var player7Info = ["name": "Sammy Adams", "height": "45", "soccerExperience": "NO", "guardianName": "Jeff Adams"]
 
-let player8Info = ["name": "Karl Saygan", "height": "42", "soccerExperience": "YES", "guardianName": "Heather Bledsoe"]
+var player8Info = ["name": "Karl Saygan", "height": "42", "soccerExperience": "YES", "guardianName": "Heather Bledsoe"]
 
-let player9Info = ["name": "Suzane Greenberg", "height": "44", "soccerExperience": "YES", "guardianName": "Henrietta Dumas"]
+var player9Info = ["name": "Suzane Greenberg", "height": "44", "soccerExperience": "YES", "guardianName": "Henrietta Dumas"]
 
-let player10Info = ["name": "Sal Dali", "height": "41", "soccerExperience": "NO", "guardianName": "Gala Dali"]
+var player10Info = ["name": "Sal Dali", "height": "41", "soccerExperience": "NO", "guardianName": "Gala Dali"]
 
-let player11Info = ["name": "Joe Kavalier", "height": "39", "soccerExperience": "NO", "guardianName": "Sam and Elaine Kavalier"]
+var player11Info = ["name": "Joe Kavalier", "height": "39", "soccerExperience": "NO", "guardianName": "Sam and Elaine Kavalier"]
 
-let player12Info = ["name": "Ben Finkelstein", "height": "44", "soccerExperience": "NO", "guardianName": "Aaron and Jill Finkelstein"]
+var player12Info = ["name": "Ben Finkelstein", "height": "44", "soccerExperience": "NO", "guardianName": "Aaron and Jill Finkelstein"]
 
-let player13Info = ["name": "Diego Soto", "height": "41", "soccerExperience": "YES", "guardianName": "Robin and Sarika Soto"]
+var player13Info = ["name": "Diego Soto", "height": "41", "soccerExperience": "YES", "guardianName": "Robin and Sarika Soto"]
 
-let player14Info = ["name": "Chloe Alaska", "height": "47", "soccerExperience": "NO", "guardianName": "David and Jamie Alaska"]
+var player14Info = ["name": "Chloe Alaska", "height": "47", "soccerExperience": "NO", "guardianName": "David and Jamie Alaska"]
 
-let player15Info = ["name": "Arnold Willis", "height": "43", "soccerExperience": "NO", "guardianName": "Claire Willis"]
+var player15Info = ["name": "Arnold Willis", "height": "43", "soccerExperience": "NO", "guardianName": "Claire Willis"]
 
-let player16Info = ["name": "Phillip Helm", "height": "44", "soccerExperience": "YES", "guardianName": "Thomas Helm and Eva Jones"]
+var player16Info = ["name": "Phillip Helm", "height": "44", "soccerExperience": "YES", "guardianName": "Thomas Helm and Eva Jones"]
 
-let player17Info = ["name": "Les Clay", "height": "42", "soccerExperience": "YES", "guardianName": "Wynonna Brown"]
+var player17Info = ["name": "Les Clay", "height": "42", "soccerExperience": "YES", "guardianName": "Wynonna Brown"]
 
-let player18Info = ["name": "Herschel Krustofski", "height": "45", "soccerExperience": "YES", "guardianName": "Hyman and Rachel Krustofski"]
+var player18Info = ["name": "Herschel Krustofski", "height": "45", "soccerExperience": "YES", "guardianName": "Hyman and Rachel Krustofski"]
+
+
+// Collection of total players within the soccer league
 
 var allPlayers = [player1Info, player2Info, player3Info, player4Info, player5Info, player6Info, player7Info, player8Info, player9Info, player10Info, player11Info, player12Info, player13Info, player14Info, player15Info, player16Info, player17Info, player18Info]
 
-//allPlayers[13]["name"]
 
-//    print(allPlayers[player]["name"])
+// This function calculates number of experienced and unexperienced players within the soccer league
 
 var experiencedPlayers = 0
 
 var unexperiencedPlayers = 0
-
-// Calculating number of experienced players
 
 func getPlayersExperience() -> (Int, Int) {
     
@@ -71,9 +71,12 @@ func getPlayersExperience() -> (Int, Int) {
     return (experiencedPlayers, unexperiencedPlayers)
 }
 
+// Calling function to get the number of experienced and unexperienced players within the soccer league
+
 getPlayersExperience()
 
-// Divide Players' Experience Evenly
+
+// Defining Teams within the soccer league
 
 var dragons = [Dictionary<String, String>]()
 
@@ -81,13 +84,19 @@ var sharks = [Dictionary<String, String>]()
 
 var raptors = [Dictionary<String, String>]()
 
+// Collection of total teams within the soccer league
+
 var teams = [dragons, sharks, raptors]
+
+// Determining an even number of experienced and unexperienced players amongst X number of teams (defined above)
 
 let experiencedPlayersInEachTeam = experiencedPlayers / teams.count
 
 let unexperiencedPlayersInEachTeam = unexperiencedPlayers / teams.count
 
 let totalPlayersInEachTeam = experiencedPlayersInEachTeam + unexperiencedPlayersInEachTeam
+
+// Counters to keep track of experienced and nonexperienced players within each team
 
 var dragonsExp = 0
 
@@ -101,42 +110,126 @@ var raptorsExp = 0
 
 var raptorsUnExp = 0
 
-for player in allPlayers {
+// This function sorts experienced and unexperienced players evenly into X number of teams (defined above)
+
+func dividePlayersEvenly() {
+
+    for player in allPlayers {
     
-    if (dragons.count < totalPlayersInEachTeam && dragonsExp < experiencedPlayersInEachTeam && player["soccerExperience"] == "YES") {
+        if (dragons.count < totalPlayersInEachTeam && dragonsExp < experiencedPlayersInEachTeam && player["soccerExperience"] == "YES") {
         
-        dragonsExp+=1
+            dragonsExp+=1
         
-        dragons.append(player)
+            dragons.append(player)
         
-    } else if (dragons.count < totalPlayersInEachTeam && dragonsUnExp < unexperiencedPlayersInEachTeam && player["soccerExperience"] == "NO") {
+        } else if (dragons.count < totalPlayersInEachTeam && dragonsUnExp < unexperiencedPlayersInEachTeam && player["soccerExperience"] == "NO") {
         
-        dragonsUnExp+=1
-        dragons.append(player)
+            dragonsUnExp+=1
+            dragons.append(player)
         
-    } else if (sharks.count < totalPlayersInEachTeam && sharksExp < experiencedPlayersInEachTeam && player["soccerExperience"] == "YES") {
+        } else if (sharks.count < totalPlayersInEachTeam && sharksExp < experiencedPlayersInEachTeam && player["soccerExperience"] == "YES") {
         
-        sharksExp+=1
-        sharks.append(player)
+            sharksExp+=1
+            sharks.append(player)
         
-    } else if (sharks.count < totalPlayersInEachTeam && sharksUnExp < unexperiencedPlayersInEachTeam && player["soccerExperience"] == "NO") {
+        } else if (sharks.count < totalPlayersInEachTeam && sharksUnExp < unexperiencedPlayersInEachTeam && player["soccerExperience"] == "NO") {
         
-        sharksUnExp+=1
-        sharks.append(player)
+            sharksUnExp+=1
+            sharks.append(player)
         
-    } else if (raptors.count < totalPlayersInEachTeam && raptorsExp < experiencedPlayersInEachTeam && player["soccerExperience"] == "YES") {
+        } else if (raptors.count < totalPlayersInEachTeam && raptorsExp < experiencedPlayersInEachTeam && player["soccerExperience"] == "YES") {
         
-        raptorsExp+=1
-        raptors.append(player)
+            raptorsExp+=1
+            raptors.append(player)
         
-    } else if (raptors.count < totalPlayersInEachTeam && raptorsUnExp < unexperiencedPlayersInEachTeam && player["soccerExperience"] == "NO") {
+        } else if (raptors.count < totalPlayersInEachTeam && raptorsUnExp < unexperiencedPlayersInEachTeam && player["soccerExperience"] == "NO") {
         
-        raptorsUnExp+=1
-        raptors.append(player)
+            raptorsUnExp+=1
+            raptors.append(player)
         
+        }
     }
-    
 }
+
+// Calling function to sort experienced and unexperienced players evenly into X number of teams (defined above)
+
+dividePlayersEvenly()
+
+
+
+//
+//for team in teams {
+//        
+//    if (team == dragons) {
+//        for player in team {
+//            
+//            print("Dear \(player["guardianName"]), \(player["name"]) is officially a part of the Dragons! We look forward to seeing you at \(player["name"])'s first practice on March 17, at 1pm!")
+//            
+//        }
+//        
+//    } else if (team == sharks) {
+//        for player in team {
+//                
+//            print("Dear \(player["guardianName"]), \(player["name"]) is officially a part of the Sharks! We look forward to seeing you at \(player["name"])'s first practice on March 17, at 3pm!")
+//            
+//        }
+//        
+//    } else {
+//        for player in team {
+//                
+//            print("Dear \(player["guardianName"]), \(player["name"]) is officially a part of the Raptors! We look forward to seeing you at \(player["name"])'s first practice on March 18, at 1pm!")
+//        }
+//        
+//    }
+//            
+//}
+
+//var index = 0
+//
+//while index <= teams.count {
+//    
+//    for team in teams {
+//    
+//        if team == dragons {
+//            for player in team {
+//                
+//                index+=1
+//        
+//            print("Dear \(player["guardianName"]), \(player["name"]) is officially a part of the Dragons! We look forward to seeing you at \(player["name"])'s first practice on March 17, at 1pm!")
+//            }
+//        
+//        } else if team == sharks {
+//            
+//            for player in team {
+//                
+//                index+=1
+//                
+//                print("Dear \(player["guardianName"]), \(player["name"]) is officially a part of the Sharks! We look forward to seeing you at \(player["name"])'s first practice on March 17, at 3pm!")
+//            }
+//            
+//        } else {
+//            
+//            for player in team {
+//                
+//                index+=1
+//            
+//                print("Dear \(player["guardianName"]), \(player["name"]) is officially a part of the Raptors! We look forward to seeing you at \(player["name"])'s first practice on March 18, at 1pm!")
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+    
+
 
 
 
